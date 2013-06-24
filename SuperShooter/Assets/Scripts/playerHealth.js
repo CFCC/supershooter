08a3dfa3380a64c80cd0509fec7945e1d75eecee
bradrules97@gmail.com <bradrules97@gmail.com@bca013d4-7c61-d76e-213c-18c973614017>
2013-06-24 22:21:17 +0000

    Improved health systems.

--- SuperShooter/Assets/Scripts/playerHealth.js
+++ SuperShooter/Assets/Scripts/playerHealth.js
@@ -3,9 +3,13 @@ var triggerTag : String;
 var destination : GameObject;
 var objectToRelocate : GameObject;
 var startHealth = 100;
+var healthGui : GUIText;
 private var currentHealth = startHealth;
 
+
+
 function Update(){
+	healthGui.text ="Health: " + currentHealth.ToString();
 	if (currentHealth <= 0){
 			
 		objectToRelocate.transform.position = destination.transform.position;
@@ -19,6 +23,7 @@ function OnCollisionEnter (c : Collision) : void{
 		if (c.gameObject.tag == triggerTag){
 			
 			currentHealth -= 10;
+			
 		}
     Debug.Log("hit~! : " + currentHealth);
 }
