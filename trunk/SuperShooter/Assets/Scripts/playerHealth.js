ca7b33395c9f39f520d07a148ddb8e83c669e8ad
bradrules97@gmail.com <bradrules97@gmail.com@bca013d4-7c61-d76e-213c-18c973614017>
2013-06-25 15:58:12 +0000

    Health update again again blood splatters work

--- trunk/SuperShooter/Assets/Scripts/playerHealth.js
+++ trunk/SuperShooter/Assets/Scripts/playerHealth.js
@@ -6,7 +6,8 @@ var startHealth = 100;
 var healthGui : GUIText;
 private var currentHealth = startHealth;
 var blood : Texture;
-
+var timeAtStart : long;
+var timeNow : long;
 
 function Update(){
 	healthGui.text ="Health: " + currentHealth.ToString();
@@ -17,22 +18,26 @@ function Update(){
 	}
 
 }
-function OnGUI(){
-	// draws on mountain and in corner
-	//Graphics.DrawTexture(Rect(10, 10, 100, 100), blood);
-	GUI.DrawTexture(Rect(10,10,100,800), blood);
 
-}
 
 function OnCollisionEnter (c : Collision) : void{
 		
 		if (c.gameObject.tag == triggerTag){
-			
+			timeAtStart = System.DateTime.Now.Ticks;
 			currentHealth -= 10;
 			
 			
 		}
 }
 						
-
+function OnGUI(){
+	// draws on mountain and in corner
+	//Graphics.DrawTexture(Rect(10, 10, 100, 100), blood);
+	Debug.Log("time at start " + timeAtStart + " " + "time now " + timeNow);
+	if((timeAtStart + 2000000) > timeNow){
+		GUI.DrawTexture(Rect(10,10,100,800), blood);
+	}
+	timeNow = System.DateTime.Now.Ticks;
+	
+}
     
