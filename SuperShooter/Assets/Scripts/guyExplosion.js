8c63ffe79d04ec7d7b101b39ebb0244a7aa6dab3
mtwmt2@gmail.com <mtwmt2@gmail.com@bca013d4-7c61-d76e-213c-18c973614017>
2013-06-25 15:16:12 +0000

    Added sound and explosion to dudes

--- SuperShooter/Assets/Scripts/guyExplosion.js
+++ SuperShooter/Assets/Scripts/guyExplosion.js
@@ -4,7 +4,39 @@ var model : GameObject;
 var colideCount = 0;
 var collided = false;
 
-function DestroyNow ()
-{
+function Start(){
+
+	Invoke("Explode", 12.2);	
+}
+function Explode(){
+	if (causesExplosion){
+		var newexplosion = Instantiate(explosion, gameObject.transform.position, gameObject.transform.rotation);
+		var radius = 75.0;
+		var power = 4000.0;
+		var explosionPos = gameObject.transform.position;
+		
+		var colliders = Physics.OverlapSphere (explosionPos, radius);
+   		
+   		for (var hit in colliders) {
+        	if (hit == null)
+	       		continue;
+	   		if (!hit.isTrigger && hit.rigidbody){
+				hit.gameObject.SendMessage("OnExplosion", explosionPos, SendMessageOptions.DontRequireReceiver );
+			}
+	   	} 	 
+	   	
+	   	colliders = Physics.OverlapSphere (explosionPos, radius); 		
+	   	
+		for (var hit in colliders) {
+ 			if (hit == null)
+       			continue;
+        	if (!hit.isTrigger && hit.rigidbody){
+				hit.rigidbody.AddExplosionForce(power, explosionPos, radius, 0);
+			}
+		}
+	}
+	Invoke ("DestroyNow", 0);
+}
+function DestroyNow(){
 	DestroyObject(gameObject);
 }
